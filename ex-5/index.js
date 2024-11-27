4a63dd299749a5abfe3367fd71a1143bfa04a785
thomasR35 <thomasriou.35@gmail.com>
2024-11-27 17:06:10 +0100

    Update index.js

--- ex-5/index.js
+++ ex-5/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", (event) => {
     let password = document.getElementById("password");
     let confirmPassword = document.getElementById("confirmPassword");
     let error = document.querySelector("form p");
     if (password.value !== confirmPassword.value) {
-      e.preventDefault();
+      event.preventDefault();
       error.classList.add("show");
     } else {
       error.classList.remove("show");
